7a351636ce4088a693df0636baa13e8e08c592d5
mshero7 <mshero7@naver.com>
2019-06-25 17:48:33 +0900

    Callback 으로 구현된 ThreadPool 활용

--- src/threadpool/CallbackExample.java
+++ src/threadpool/CallbackExample.java
@@ -16,15 +16,39 @@ public class CallbackExample {
 
 				@Override
 				public void failed(Throwable exc, Void attachment) {
-					System.out.println("Completed() ½ÇÇà " + exc.toString());
+					System.out.println("Failed() ½ÇÇà " + exc.toString());
 				}
 	};
 	
 	public void doWork(final String x, final String y) {
-		
+		Runnable task = new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					int intX = Integer.parseInt(x);
+					int intY = Integer.parseInt(y);
+					int result = intX + intY;
+					
+					callback.completed(result, null);
+				} catch (Exception e) {
+					callback.failed(e, null);
+				}
+				
+			}
+		};
+		executorService.submit(task);
+	}
+	
+	public void finish() {
+		executorService.shutdown();
 	}
 	
 	public static void main(String[] args) {
+		CallbackExample callbackExample = new CallbackExample();
+		callbackExample.doWork("3", "3");
+		callbackExample.doWork("3", "»ï");
+		callbackExample.finish();
 		
 	}
 }
